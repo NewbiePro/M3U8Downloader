4091669e68ef79e139d26a57c7e1749e7a650e1a
RookieMaster <a2383779481@gmail.com>
2025-05-18 17:37:34 +0800

    progress bar update

--- src/main/java/com/tech/newbie/m3u8downloader/model/Video.java
+++ src/main/java/com/tech/newbie/m3u8downloader/model/Video.java
@@ -5,14 +5,21 @@ import java.io.File;
 public class Video {
     private final String fileName;
     private final String path;
+    private File file;
+    private long durationMillis;
+    private long currentMillis;
 
     public Video(String fileName, String path) {
         this.fileName = fileName;
         this.path = path;
+        file = new File(path , fileName+ ".mp4");
     }
 
     public File getFile() {
-        return new File(path , fileName+ ".mp4");
+        if(file == null){
+            return new File(path , fileName+ ".mp4");
+        }
+        return file;
     }
 
     public boolean exists(){
